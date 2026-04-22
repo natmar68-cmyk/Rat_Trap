04a7b27fc5cb86cfe7c7730fca0fa46fb207f1bb
nichar94 <nichar94@kingshigh.school.nz>
2026-04-22 15:01:01 +1200

    Progress on Food. Doesn't work

--- Scripts/food_item.gd
+++ Scripts/food_item.gd
@@ -3,14 +3,11 @@ extends RigidBody3D
 @export var food_name: String = "Cheese"
 var collected: bool = false
 
-signal food_collected(food_name)
-
 func _ready():
 	$Area3D.body_entered.connect(_on_pickup_zone_entered)
 
 func _on_pickup_zone_entered(body):
-	if body.is_in_group("rat") and not collected:
+	if body.is_in_group("Player") and not collected:
 		collected = true
-		emit_signal("food_collected", food_name)
-		queue_free()  # Remove from scene
-#Sort out the spawning food in kitchen
+		GameManager.collect_food(food_name)
+		queue_free()

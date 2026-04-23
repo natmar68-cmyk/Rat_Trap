104ea20acbfe95aebcee3520ebd5cf73252b006d
natmar68-cmyk <natmar68@kingshigh.school.nz>
2026-04-24 09:17:55 +1200

    Fixed Collisions

--- FoodLabel.gd
+++ FoodLabel.gd
@@ -2,7 +2,8 @@ extends CanvasLayer
 
 func _ready():
 	GameManager.food_count_changed.connect(_on_food_count_changed)
-	$FoodLabel.text = "Food: 0"
+	#$FoodLabel.text = "Food: 0"
 
 func _on_food_count_changed(new_count):
-	$FoodLabel.text = "Food: " + str(new_count)
+	pass
+	#$FoodLabel.text = "Food: " + str(new_count)

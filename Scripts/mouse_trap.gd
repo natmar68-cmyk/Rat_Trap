b9344d84da4a989f42ab8a54cad27c9e0df61fcc
nichar94 <nichar94@kingshigh.school.nz>
2026-04-29 11:55:51 +1200

    trap works, death works

--- Scripts/mouse_trap.gd
+++ Scripts/mouse_trap.gd
@@ -5,4 +5,5 @@ func _ready():
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://death_screen.tscn")
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().change_scene_to_file("res://scenes/death_screen.tscn")

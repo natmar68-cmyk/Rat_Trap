c38318a16dac991b369e20690626570323058943
Domit011 <nathanmaree11nz@gmail.com>
2026-07-30 12:49:42 +1200

    Menus are mostly done, just have to wait for level to be complete to replace background. Adde a loading screen and fixed lag

--- Scripts/DeathScreen.gd
+++ Scripts/DeathScreen.gd
@@ -1,17 +1,20 @@
 extends CanvasLayer
 
 func _ready() -> void:
-	$Button.pressed.connect(_on_retry_pressed)
+	$RetryButton.pressed.connect(_on_retry_pressed)
 	AudioManager.muffle()
 
 func _on_retry_pressed() -> void:
 	AudioManager.unmuffle()
 	GameManager.reset()
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	get_tree().change_scene_to_file("res://Scenes/kitchen.tscn")
+	
+	# Replace change_scene_to_file with your new Autoload
+	LoadingScreen.load_scene("res://Scenes/kitchen.tscn")
+
 func _on_main_menu_pressed() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	LoadingScreen.load_scene("res://Scenes/menu.tscn")
 
 func _on_button_2_pressed() -> void:
 	pass # Replace with function body.

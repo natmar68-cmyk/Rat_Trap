3e2e439cd66fbf3b3f53b276274f93d9c8bff394
Nicobruuuh <nichar94@kingshigh.school.nz>
2026-08-10 14:59:25 +1200

    start to delivery fix

--- Scripts/rat_hole_entrance.gd
+++ Scripts/rat_hole_entrance.gd
@@ -4,5 +4,5 @@ func _ready():
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	if body.is_in_group("Player") and GameManager.food_carried > 0:
+	if body.is_in_group("player") and GameManager.food_carried > 0:
 		GameManager.deliver_food()
